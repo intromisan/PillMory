9e4f23575656a2eb1ae36b6fdfc850ed7b4120a6
Serge Najimov <serge.najimov@peruzziservices.com>
2022-02-01 17:44:12 +0100

    started navigation tabs

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import NavigationTabs from "../components/Navigation/NavigationTabs";
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>Home Screen</Text>
+      {/* <NavigationTabs /> */}
     </View>
   );
 };
